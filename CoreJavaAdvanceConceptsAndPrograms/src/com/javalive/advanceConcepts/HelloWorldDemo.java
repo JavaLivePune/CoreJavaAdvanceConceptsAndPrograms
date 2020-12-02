dabedfa67a898385e85ff350dfe981d1547bcfa7
abc <abc@xyz.com>
2020-12-02 12:32:06 +0530

    Merged result.

--- CoreJavaAdvanceConceptsAndPrograms/src/com/javalive/advanceConcepts/HelloWorldDemo.java
+++ CoreJavaAdvanceConceptsAndPrograms/src/com/javalive/advanceConcepts/HelloWorldDemo.java
@@ -3,5 +3,6 @@ package com.javalive.advanceConcepts;
 public class HelloWorldDemo {
 public static void main(String[] args) {
 	System.out.println("Hello World222222!!!!!");
+	System.out.println("Ksdsdsdsd");
 }
 }
