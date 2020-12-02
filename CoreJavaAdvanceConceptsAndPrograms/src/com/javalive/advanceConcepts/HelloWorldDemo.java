9d154f124449831d503e4a224e3ca5c363e55812
abc <abc@xyz.com>
2020-12-02 13:21:14 +0530

    Commit from NewWorkspace

--- CoreJavaAdvanceConceptsAndPrograms/src/com/javalive/advanceConcepts/HelloWorldDemo.java
+++ CoreJavaAdvanceConceptsAndPrograms/src/com/javalive/advanceConcepts/HelloWorldDemo.java
@@ -3,6 +3,6 @@ package com.javalive.advanceConcepts;
 public class HelloWorldDemo {
 public static void main(String[] args) {
 	System.out.println("Hello World222222!!!!!");
-	System.out.println("Ksdsdsdsd");
+	System.out.println("Ksdsdsdsd12424");
 }
 }
